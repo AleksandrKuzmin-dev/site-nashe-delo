b402242845e9d0d4b9cea338a5680e391118cede
Aleksandr Kuzmin <a-kuzmin.dev@yandex.ru>
2024-04-18 20:16:27 +0400

    Верстка страница /Контакты/ и модального окна

--- js/script.js
+++ js/script.js
@@ -137,6 +137,57 @@ function setDropDownCards(tabsSelector, activeTabSelector) {
     });
 };
 
+function setModal(triggerSelector, modalSelector, closeSelector, timerValue) {
+    const trigger = document.querySelector(triggerSelector),
+          modal = document.querySelector(modalSelector),
+          close = document.querySelector(closeSelector),
+          body = document.querySelector('body');
+    
+    let isOpen = false;
+    let timeOut = null;
+
+    const showModal = () => {
+        modal.classList.remove('none');
+        isOpen = true;
+        body.style.overflow = 'hidden';
+        timeOut = null;
+    };
+
+    const closeModal = () => {
+        modal.classList.add('none');
+        isOpen = false;
+        body.style.overflow = 'unset';
+    };
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && isOpen) {
+            closeModal();
+        };
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    })
+
+    if (trigger) {
+        trigger.addEventListener('click', () => {
+            showModal();
+        });
+    };
+
+    close.addEventListener('click', () => {
+        closeModal();
+    });
+
+    if (timerValue) {
+        timeOut = setTimeout(() => {
+            showModal();
+        }, timerValue);
+    };
+};
+
 SmoothScroll({
     // Время скролла 400 = 0.4 секунды
     animationTime    : 800,
